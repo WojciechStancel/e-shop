1e6997857099fa7bba8269f584ee247b1565db59
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-01 17:05:01 +0100

    added bestsellers feature

--- front-end/src/components/BestSellers/BestSellers.jsx
+++ front-end/src/components/BestSellers/BestSellers.jsx
@@ -1,11 +1,12 @@
+import style from "./BestSellers.module.css";
 import CenteredContent from "../CenteredContent/CenteredContent";
 import Product from "../Product/Product";
 
 const BestSellers = ({ products }) => {
 	return (
 		<CenteredContent>
-			<h2>Bestsellery</h2>
-			<div>
+			<h2 className={style.bestsellersHeader}>Bestsellery</h2>
+			<div className={style.productsWrapper}>
 				{products.map((product) => {
 					return <Product key={product.id} product={product} />;
 				})}
--- front-end/src/components/Product/Product.jsx
+++ front-end/src/components/Product/Product.jsx
@@ -1,6 +1,14 @@
-const Product = ({product}) => {
-  return (
-    <p>{product.productName}</p>
-  )
-}
-export default Product
+import style from "./Product.module.css";
+import { Link } from "react-router-dom";
+
+const Product = ({ product }) => {
+	return (
+		<Link className={style.product}>
+			<img src={product.photos[0]} />
+			<h3>{product.productName}</h3>
+			<p>{product.pricePLN}</p>
+            <div className={style.heart}></div>
+		</Link>
+	);
+};
+export default Product;
