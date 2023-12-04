90c59224f664d7a506397e6929fb7f2f00cc9d4c
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-12-04 01:06:54 +0100

    feature pagination

---
+++ front-end/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,18 @@
+import styles from "./Pagination.module.css";
+import { NavLink } from "react-router-dom";
+
+const Pagination = ({ numberOfPages }) => {
+	const pages = Array(numberOfPages).fill(null);
+	return (
+		<ul className={styles.pagination}>
+			{pages.map((page, index) => {
+				return (
+					<li key={index}>
+						<NavLink>{index + 1}</NavLink>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
+export default Pagination;
